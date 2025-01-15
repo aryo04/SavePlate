2f226c5a19b7d59910a7dd8a27704017278dbce1
gadingrdtya <gadingraditya77@gmail.com>
2025-01-15 17:19:07 +0700

    update: about us

--- pages/restoranrotidankue.php
+++ pages/restoranrotidankue.php
@@ -4,35 +4,35 @@
         <section class="content">
             <h2>Restoran</h2>
             <div class="resto-grid">
-                <a href="../pages/makananberat.php" class="resto-item">
-                    <h2>Pagi Sore</h2>
-                    <img src="../images/pagisore.png" alt="Pagi Sore">
+                <a href="../pages/rotidankue.php" class="resto-item">
+                    <h2>Breadtalk</h2>
+                    <img src="../images/breadtalk.png" alt="Breadtalk">
                 </a>
-                <a href="../pages/makananberat.php" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Nasi Uduk</h2>
                     <img src="../images/nasiuduk.png" alt="Nasi Uduk">
                 </a>
-                <a href="ayam_geprek.html" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Ayam Geprek</h2>
                     <img src="../images/ayamgeprek.png" alt="Ayam Geprek">
                 </a>
-                <a href="bakso.html" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Bakso</h2>
                     <img src="../images/bakso.png" alt="Bakso">
                 </a>
-                <a href="mie_ayam.html" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Mie Ayam</h2>
                     <img src="../images/mieayam.png" alt="Mie Ayam">
                 </a>
-                <a href="sate_padang.html" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Sate Padang</h2>
                     <img src="../images/satepadang.png" alt="Sate Padang">
                 </a>
-                <a href="ketoprak.html" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Ketoprak</h2>
                     <img src="../images/ketoprak.png" alt="Ketoprak">
                 </a>
-                <a href="gado_gado.html" class="resto-item">
+                <a href="../pages/rotidankue.php" class="resto-item">
                     <h2>Gado-Gado</h2>
                     <img src="../images/gadogado.png" alt="Gado-Gado">
                 </a>
